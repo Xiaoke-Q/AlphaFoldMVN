15ad6f2839819b6b3c44fa7adcfb85662357ba37
xtan25 <82909456+xtan25@users.noreply.github.com>
2025-11-02 23:52:42 -0600

    Update Descriptions

--- R/helmert.R
+++ R/helmert.R
@@ -1,3 +1,12 @@
+#' Generate an orthonormal Helmertian matrix in the strict sense, without the first row.
+#'
+#' @param d The number of columns of the matrix.
+#'
+#' @returns A (d-1)-by-d Helmertian matrix.
+#'
+#' @export
+#' @examples
+#' helmert(d = 4)
 helmert <- function(d){
   stopifnot(d >= 2L)
   mc <- contr.helmert(d)
